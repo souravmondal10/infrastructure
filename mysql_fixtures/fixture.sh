b1b1f9f60843279a7da29a435a4b7bbaaeab24b3
sourav mondal <souravmondal10@gmail.com>
2021-06-14 16:09:00 +0000

    nginx config change

---
+++ mysql_fixtures/fixture.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -u root -p'very_strong_passowrd' UserBase < fixture.sql
---
+++ mysql_fixtures/fixture.sql
@@ -0,0 +1,9 @@
+use UserBase;
+
+CREATE TABLE `users` (
+  `userId` varchar(50) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `useremail` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `users` ADD PRIMARY KEY (`userId`);
