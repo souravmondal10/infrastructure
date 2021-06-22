446f9f2636b4d0a45d1437749eb4b2458d9c95c1
sourav mondal <souravmondal10@gmail.com>
2021-06-22 00:41:59 +0000

    updated latest code

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('REDIS_HOST', 'redis');
-define('REDIS_PORT', 6379);
-define('MYSQL_HOST', 'mysql');
-define('MYSQL_PORT', '3306');
-define('MYSQL_DATABASE', 'UserBase');
-define('MYSQL_USER', 'root');
-define('MYSQL_PASSWORD', 'very_strong_password');
